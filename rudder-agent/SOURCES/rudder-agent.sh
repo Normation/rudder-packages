c7979a19f40b374bbbd1c4bfb69e6aa193fbb37e
Matthieu CERDA <matthieu.cerda@normation.com>
2015-04-30 16:33:31 +0200

    Fixes #6548: Use the manpath command if available to build a MANPATH in profile

--- rudder-agent/SOURCES/rudder-agent.sh
+++ rudder-agent/SOURCES/rudder-agent.sh
@@ -1,13 +1,23 @@
-# Add CFEngine binaries to the PATH
+# Rudder profile entries, to add the agent to the
+# PATH and its manpages to the MANPATH
+
+# 1 - Add CFEngine binaries to the PATH
+
 PATH=${PATH}:/var/rudder/cfengine-community/bin
 export PATH
 
-# Add CFEngine manpages to the MANPATH
-if [ -z "${MANPATH}" ]
+# 2 - Build a MANPATH with our manpages in it
+
+## If $MANPATH is already defined, use it, or build
+## a new one from scratch if it does not
+if type manpath >/dev/null 2>&1
 then
-    MANPATH=/opt/rudder/share/man
+    MANPATH=$(manpath -q):/opt/rudder/share/man
+elif [ ! -z "${MANPATH}" ]
+then
+    MANPATH=${MANPATH}:/opt/rudder/share/man
 else
-    MANPATH=/opt/rudder/share/man:${MANPATH}
+    MANPATH=/opt/rudder/share/man
 fi
 
 export MANPATH
