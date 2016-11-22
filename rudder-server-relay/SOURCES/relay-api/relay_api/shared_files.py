fadc5cf082aac8088a199a5451a8ab040ec153e3
Benoît Peccatte <benoit.peccatte@normation.com>
2016-11-22 10:50:16 +0100

    Fixes #9708: Create a relay API for shared-files

---
+++ rudder-server-relay/SOURCES/relay-api/relay_api/shared_files.py
@@ -0,0 +1,289 @@
+from relay_api.common import *
+
+import base64
+import re
+import os
+import datetime
+import requests
+
+# disable ssl warning on rudder connection in all the possible ways
+try:
+  import urllib3
+  urllib3.disable_warnings()
+except:
+  pass
+
+try:
+  requests.packages.urllib3.disable_warnings()
+except:
+  pass
+
+from pprint import pprint
+from Crypto.Hash import *
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+
+
+SIGNATURE_FORMAT="header=rudder-signature-v1"
+METADATA_EXTENSION=".metadata"
+
+# convert a byte string to hexadecimal representation
+toHex = lambda x:"".join([hex(ord(c))[2:].zfill(2) for c in x])
+
+# convert an hexadecimal string to a byte string
+toBin = lambda x:"".join([chr(int(x[c:c+2],16)) for c in range(0,len(x),2)])
+
+# Parse a ttl string of the form "1day 2hours 3minute 4seconds"
+# It can be abreviated to thos form "5h 3s"
+# If it is a pure integer it is considered to be seconds
+# Returns a timedelta object
+def parse_ttl(string):
+  m = re.match(r'^\s*(\d+)\s*$', string)
+  if m:
+    days = 0
+    seconds = int(m.group(1))
+  else:
+    daymatch  = r'(\d+)d(?:ays?)?'
+    hourmatch = r'(\d+)h(?:ours?)?'
+    minmatch  = r'(\d+)m(?:inutes?)?'
+    secmatch  = r'(\d+)s(?:econds?)?'
+    match = r'^\s*(?:' + daymatch + r'\s*)?(?:' + hourmatch + r'\s*)?(?:' + minmatch + r'\s*)?(?:' + secmatch + r'\s*)?$'
+    m = re.match(match, string)
+    if m:
+      days = 0
+      seconds = 0
+      if m.group(1) is not None:
+        days = int(m.group(1))
+      if m.group(2) is not None:
+        seconds += 3600 * int(m.group(2))
+      if m.group(3) is not None:
+        seconds += 60 * int(m.group(3))
+      if m.group(4) is not None:
+        seconds += int(m.group(4))
+    else:
+      raise ValueError("ERROR invalid TTL specification:" + string)
+  return datetime.timedelta(days, seconds)
+
+
+# Extract the signature header from a data stream
+# The header is delimited by an empty line
+def get_header(data_stream):
+  # format parsing
+  line = data_stream.readline()
+  if line.rstrip() != SIGNATURE_FORMAT:
+    raise ValueError("ERROR unknown signature format: " + str(line))
+  header = line
+  while True:
+    line = data_stream.readline()
+    if line == "\n":
+      return header
+    header += line
+  # the return is just above
+
+
+# Extract informations from header
+def parse_header(header):
+  data = {}
+  for line in header.rstrip().split():
+    m = re.match(r"(\w+)\s*=\s*(.*)", line)
+    if m:
+      data[m.group(1)] = m.group(2)
+    else:
+      raise ValueError("ERROR invalid format: " + line)
+  return data
+
+
+# Extract a public key object from headers
+def get_pubkey(header_info):
+  # validate header content first
+  if 'digest' not in header_info or 'pubkey' not in header_info:
+    raise ValueError("ERROR incomplete header, missing digest or public key")
+  pem = "-----BEGIN RSA PRIVATE KEY-----\n" + header_info['pubkey'] + "\n-----END RSA PRIVATE KEY-----\n"
+  return RSA.importKey(pem)
+
+
+# Create expiry header line
+def expiry_line(header_info):
+  if 'ttl' not in header_info:
+    raise ValueError("ERROR: No TTL provided")
+  else:
+    ttl = parse_ttl(header_info['ttl'])
+    expires = datetime.datetime.utcnow() + ttl # we take utcnow because we write a unix timestamp
+    timestamp = int((expires - datetime.datetime(1970, 1, 1)).total_seconds()) # convert to unix timestamp
+    return "expires=" + str(timestamp) + "\n"
+    
+
+# Hash a message with a given algorithm
+# Returns a hash object
+def get_hash(algorithm, message):
+  if algorithm == "sha1":
+    h=SHA.new(message)
+  elif algorithm == "sha256":
+    h=SHA256.new(message)
+  elif algorithm == "sha512":
+    h=SHA512.new(message)
+  else:
+    raise ValueError("ERROR unknown key hash type: " + str(algorithm))
+  return h
+
+
+# Validate that a given public key matches the provided hash
+# The public key is a key object and the hash is of the form 'algorithm:kex_value'
+def validate_key(pubkey, keyhash):
+  try:
+    (keyhash_type, keyhash_value) = keyhash.split(":",1)
+  except:
+    raise ValueError("ERROR invalid key hash, it should be 'type:value': " + keyhash)
+  pubkey_bin = pubkey.exportKey(format="DER")
+  h = get_hash(keyhash_type, pubkey_bin)
+  return h.hexdigest() == keyhash_value
+
+# Validate that a message has been properly signed by the given key
+# The public key is a key object, algorithm is the hash algorithm and digest the hex signature
+# The key algorithm will always be RSA because is is loaded as such
+# Returns a booleas for the validity and the message hash to avoid computing it twice
+def validate_message(message, pubkey, algorithm, digest):
+  h = get_hash(algorithm, message)
+  cipher = PKCS1_v1_5.new(pubkey)
+  return (cipher.verify(h, toBin(digest)), h.hexdigest())
+
+
+# Find in which directory a shared file should be stored
+def file_directory(shared_path, nodes, my_uuid, target_uuid, source_uuid, file_id):
+  if not re.match(r"^[\w\-]+$", file_id):
+    raise ValueError("ERROR file_id must be an identifier [A-z0-9_-]: " + str(file_id))
+  route_path = '/shared-files/'.join(node_route(nodes, my_uuid, target_uuid))
+  return shared_path + "/" + route_path + "/files/" + source_uuid
+
+
+# Returns the stored hash from the metadata file
+def get_metadata_hash(metadata_file):
+  fd = open(metadata_file, 'r')
+  line = fd.readline().rstrip()
+  if line != SIGNATURE_FORMAT:
+    fd.close()
+    raise ValueError("ERROR invalid storage: " + line)
+  while True:
+    line = fd.readline().rstrip()
+    m = re.match(r"(\w+)\s*=\s*(.*)", line)
+    if m:
+      if m.group(1) == "hash_value":
+        fd.close()
+        return m.group(2)
+    else:
+      fd.close()
+      raise ValueError("ERROR invalid storage: " + line)
+
+
+# =====================
+#  Manage PUT API call
+# =====================
+# Parameters:
+# - target_uuid  where to send the file to
+# - source_uuid  who sent the file
+# - file_id      under which name to store the file
+# - data         the file content
+# - nodes        the content of the nodes_list file
+# - my_uuid      uuid of the current relay (self)
+# - shared_path  the shared-files directory path
+# Returns the full path of the created file
+def shared_files_put(target_uuid, source_uuid, file_id, data_stream, nodes, my_uuid, shared_path):
+  header = get_header(data_stream)
+  info = parse_header(header)
+
+  # extract information
+  pubkey = get_pubkey(info)
+  if source_uuid not in nodes:
+    raise ValueError("ERROR unknown source node: " + str(source_uuid))
+  if "keyhash" not in nodes[source_uuid]:
+    raise ValueError("ERROR invalid nodes file on the server for " + source_uuid)
+  keyhash = nodes[source_uuid]["keyhash"]
+
+  # validate key
+  if not validate_key(pubkey, keyhash):
+    raise ValueError("ERROR invalid public key or not matching UUID")
+
+  # validate message
+  message = data_stream.read()
+  (validated, message_hash) = validate_message(message, pubkey, info['algorithm'], info['digest'])
+  if not validated:
+    raise ValueError("ERROR invalid signature")
+
+  # add headers
+  header += expiry_line(info)
+  header += "hash_value=" + message_hash + "\n"
+
+  # where to store file
+  path = file_directory(shared_path, nodes, my_uuid, target_uuid, source_uuid, file_id)
+  filename = path + "/" + file_id
+
+  # write data & metadata
+  try:
+    os.makedirs(path, 0750)
+  except:
+    pass # makedirs fails if the directory exists
+  fd = open(filename, 'w')
+  fd.write(message)
+  fd.close()
+  fd = open(filename + METADATA_EXTENSION, 'w')
+  fd.write(header)
+  fd.close()
+  return filename
+
+
+# ======================
+#  Forward PUT API call
+# ======================
+# Parameters:
+# - stream     stream of posted data
+# - url        where to forward to
+# Returns the full path of the created file
+def shared_files_put_forward(stream, url):
+  # This is needed to make requests know the length
+  # It will avoid streaming the content which would make the next hop fail
+  stream.len = stream.limit
+  req = requests.put(url, data=stream, verify=False)
+  if req.status_code == 200:
+    return req.text
+  else:
+    raise ValueError("Upstream Error: " + req.text)
+
+# ======================
+#  Manage HEAD API call
+# ======================
+# Parameters:
+# - target_uuid  where to send the file to
+# - source_uuid  who sent the file
+# - file_id      under which name to store the file
+# - file_hash    the hash to compare with
+# - nodes        the content of the nodes_list file
+# - my_uuid      uuid of the current relay (self)
+# - shared_path  the shared-files directory path
+# Returns true of false
+def shared_files_head(target_uuid, source_uuid, file_id, file_hash, nodes, my_uuid, shared_path):
+  # where to find file
+  path = file_directory(shared_path, nodes, my_uuid, target_uuid, source_uuid, file_id)
+  filename = path + "/" + file_id
+  metadata = filename + METADATA_EXTENSION
+
+  # check if the file and signature exist
+  if not os.path.isfile(filename) or not os.path.isfile(metadata):
+    return False
+
+  # check hash from metadata
+  hash_value = get_metadata_hash(metadata)
+  return hash_value == file_hash
+
+# =======================
+#  Forward HEAD API call
+# =======================
+# Parameters:
+# - url     where to forward to
+# Returns true of false
+def shared_files_head_forward(url):
+  req = requests.head(url, verify=False)
+  if req.status_code == 200:
+    return True
+  if req.status_code == 404:
+    return False
+  raise ValueError("ERROR from server:" + str(req.status_code))
---
+++ rudder-server-relay/SOURCES/relay-api/relay_api/views.py
@@ -0,0 +1,75 @@
+from relay_api import app
+from relay_api.shared_files import shared_files_put, shared_files_head, shared_files_put_forward, shared_files_head_forward
+from relay_api.common import *
+
+from flask import Flask, jsonify, request, abort, make_response
+from StringIO import StringIO
+import traceback
+
+from pprint import pprint
+
+NODESLIST_FILE = "/var/rudder/cfengine-community/inputs/distributePolicy/1.0/nodeslist.json"
+UUID_FILE = '/opt/rudder/etc/uuid.hive'
+API_DEBUGINFO = True
+POLICY_SERVER_FILE = "/var/rudder/cfengine-community/policy_server.dat"
+SHARED_FILES_PATH = "/var/rudder/shared-files"
+
+#################
+# API functions #
+#################
+
+@app.route('/shared-files/<string:target_uuid>/<string:source_uuid>/<string:file_id>', methods=['PUT'])
+def put_file(target_uuid, source_uuid, file_id):
+  try:
+    nodes = get_nodes_list(NODESLIST_FILE)
+    if target_uuid not in nodes:
+      # forward the file if the node is unknown
+      policy_server = get_file_content(POLICY_SERVER_FILE)
+      if policy_server == "root":
+        return format_response("Unknown UUID: "+target_uuid, 404)
+      else:
+        url = "https://"+policy_server+"/rudder/relay-api/shared-files/" + target_uuid + "/" + source_uuid + "/" + file_id + "?hash=" + file_hash
+        res = shared_files_put_forward(request.stream, url)
+    else:
+      # process the file if it is known
+      my_uuid = get_file_content(UUID_FILE)
+      filename = shared_files_put(target_uuid, source_uuid, file_id, request.stream, nodes, my_uuid, SHARED_FILES_PATH)
+      if API_DEBUGINFO:
+        res = "OK\nWritten to: " + filename + "\n"
+      else:
+        res = "OK\n"
+    return format_response(res)
+  except Exception as e:
+    return format_error(e, API_DEBUGINFO)
+
+
+@app.route('/shared-files/<string:target_uuid>/<string:source_uuid>/<string:file_id>', methods=['HEAD'])
+def head_file(target_uuid, source_uuid, file_id):
+  try:
+    nodes = get_nodes_list(NODESLIST_FILE)
+    file_hash = request.args.get('hash', '')
+    if target_uuid not in nodes:
+      # forward the request if the node is unknown
+      policy_server = get_file_content(POLICY_SERVER_FILE)
+      if policy_server == "root":
+        return format_response("Unknown UUID: "+target_uuid, 404)
+      else:
+        url = "https://"+policy_server+"/rudder/relay-api/shared-files/" + target_uuid + "/" + source_uuid + "/" + file_id + "?hash=" + file_hash
+        res = shared_files_head_forward(url)
+    else:
+      # process the request if it is known
+      my_uuid = get_file_content(UUID_FILE)
+      res = shared_files_head(target_uuid, source_uuid, file_id, file_hash, nodes, my_uuid, SHARED_FILES_PATH)
+    if res:
+      return format_response("", 200)
+    else:
+      return format_response("", 404)
+  except Exception as e:
+    print(traceback.format_exc())
+    return format_error(e, API_DEBUGINFO)
+
+
+# main
+if __name__ == '__main__':
+  app.run(debug = True)
+
